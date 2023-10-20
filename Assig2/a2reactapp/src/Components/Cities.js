38d6b4934e496fa92cf0604ff30e86d9f0fa814d
Devanshi <devanshiapatel2003@gmail.com>
2023-10-20 19:07:26 +1030

    Create Cities Component with title

---
+++ Assig2/a2reactapp/src/Components/Cities.js
@@ -0,0 +1,8 @@
+const Cities = ({ }) => {
+    return (
+        <div className="card col-4 mb-2" style={{ width: 18 + 'rem' }}>
+            <h5 className="card-title">Cities Page</h5>
+        </div>
+    )
+}
+export default Cities
