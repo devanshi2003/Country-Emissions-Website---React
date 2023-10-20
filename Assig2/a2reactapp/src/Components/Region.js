e6bcdad1951cb34c01d6ede466618ca9fa942215
Devanshi <devanshiapatel2003@gmail.com>
2023-10-20 20:01:27 +1030

    Region.js: Display the Regions

--- Assig2/a2reactapp/src/Components/Region.js
+++ Assig2/a2reactapp/src/Components/Region.js
@@ -1,8 +1,29 @@
+import regionsData from '../TestData/regions'
+import RegionCard from './RegionCard'
+
+
 const Region = ({ }) => {
     return (
-        <div className="card col-4 mb-2" style={{ width: 18 + 'rem' }}>
-            <h5 className="card-title">Regions Page</h5>
+        <div>
+            <div className="card col-4 mb-2" style={{ width: 18 + 'rem' }}>
+                <h5 className="card-title">Regions Page</h5>
+            </div>
+
+            <div className="row">
+                {regionsData.map((obj) => (
+                    <RegionCard
+                        key={obj.regionId}
+                        regionId={obj.regionId}
+                        regionName={obj.regionName}
+                        imageUrl={obj.imageUrl}
+                        countryCount={obj.countryCount}
+                    />
+                )
+                )
+                }
+            </div>
         </div>
+        
     )
 }
 export default Region
