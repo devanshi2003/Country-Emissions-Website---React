299014431c250de2499e9767107c32cf08405854
Devanshi <devanshiapatel2003@gmail.com>
2023-10-20 13:34:57 +1030

    Create Region Component with title

---
+++ Assig2/a2reactapp/src/Components/Region.js
@@ -0,0 +1,8 @@
+const Region = ({ }) => {
+    return (
+        <div className="card col-4 mb-2" style={{ width: 18 + 'rem' }}>
+            <h5 className="card-title">Regions Page</h5>
+        </div>
+    )
+}
+export default Region
