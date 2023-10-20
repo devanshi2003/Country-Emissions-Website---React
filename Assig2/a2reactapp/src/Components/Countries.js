a30a911ffb3fbac588a45367d17ccfe82aaaaa2f
Devanshi <devanshiapatel2003@gmail.com>
2023-10-20 13:44:07 +1030

    Create Countries Component with title

---
+++ Assig2/a2reactapp/src/Components/Countries.js
@@ -0,0 +1,8 @@
+const Countries = ({ }) => {
+    return (
+        <div className="card col-4 mb-2" style={{ width: 18 + 'rem' }}>
+            <h5 className="card-title">Countries Page</h5>
+        </div>
+    )
+}
+export default Countries
