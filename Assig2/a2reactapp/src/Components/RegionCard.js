3783f995f236bc4d85e60bdf4c5085524cbcf18f
Devanshi <devanshiapatel2003@gmail.com>
2023-10-20 20:24:12 +1030

    RegionCard: Direct to another page when region card is selected

--- Assig2/a2reactapp/src/Components/RegionCard.js
+++ Assig2/a2reactapp/src/Components/RegionCard.js
@@ -1,19 +1,26 @@
-
+import { Link } from "react-router-dom";
 
 const RegionCard = ({ regionId, regionName, imageUrl, countryCount }) => {
 
-    return (
-        <div className="card" style={{ width: 18 + 'rem' }} >
-            <img src={imageUrl} className="card-img-top" alt={"Image of " + regionName} />
-            <div className="card-body">
-                <h5 className="card-title">{regionName}</h5>
-                <h6 className="card-title">{regionId}</h6>
-                <p className="card-text">{countryCount}</p>
-                </div>
-        </div>
-    )
+    const linkStyle = {
+        textDecoration: 'none', 
+        color: 'inherit', 
+    };
 
-}
+    return (
+        
+        <div className="card" style={{ width: '18rem' }}>
+            <Link to="/" style={linkStyle}>
+                <img src={imageUrl} className="card-img-top" alt={"Image of " + regionName} />
+                <div className="card-body">
+                    <h5 className="card-title">{regionName}</h5>
+                    <h6 className="card-title">{regionId}</h6>
+                    <p className="card-text">{countryCount}</p>
+                </div>
+            </Link>
+        </div>
+    );
+};
 
 export default RegionCard;
 
