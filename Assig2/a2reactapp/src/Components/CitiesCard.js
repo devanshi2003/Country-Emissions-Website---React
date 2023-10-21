44aab2cbaa3456f42a0549279b2dc2d956c25445
Devanshi <devanshiapatel2003@gmail.com>
2023-10-22 09:34:46 +1030

    CitiesCard: Display all the available data

--- Assig2/a2reactapp/src/Components/CitiesCard.js
+++ Assig2/a2reactapp/src/Components/CitiesCard.js
@@ -1,10 +1,20 @@
+import {Link} from "react-router-dom"
 
-const citiesCard = ({ cityName }) => {
+const citiesCard = ({ cityName, recordCount, airQualityYearRange }) => {
 
     return (
         <div className="card" style={{ width: '18rem' }}>
                 <div className="card-body">
                 <h5 className="card-title">{cityName}</h5>
+
+                {recordCount === 0
+                    ? <p className="card-text">No Air Quality Data {recordCount}</p>
+                    : <div>
+                        <p className="card-text">Total Air Quality Records: {recordCount}</p>
+                        <Link class="btn btn-primary mb-2" to={"/Cities"}>View Air Quality Data</Link>
+                       </div>
+                }
+                
                 </div>
         </div>
     )
