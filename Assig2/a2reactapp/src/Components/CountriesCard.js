25c389a9fd514922ff9db8dea831f3ce80f9cbe8
Devanshi <devanshiapatel2003@gmail.com>
2023-10-21 16:56:50 +1030

    CountriesCard.js: Ensure all available data is shown

--- Assig2/a2reactapp/src/Components/CountriesCard.js
+++ Assig2/a2reactapp/src/Components/CountriesCard.js
@@ -1,11 +1,23 @@
-const countriesCard = ({ countryName, regionName }) => {
+const countriesCard = ({ countryName, cityCount, imageUrl, iso3, emissionDataYearRange, temperatureDataYearRange }) => {
 
     return (
         <div className="card" style={{ width: '18rem' }}>
-                <div className="card-body">
+            <div className="card-body">
+                <img src={imageUrl} className="card-img-top" alt={"Image of " + countryName} />
                 <h5 className="card-title">{countryName}</h5>
-                <h6 className="card-title">{regionName}</h6>
+                <p className="card-text">Number of Cities: {cityCount}</p>
+                <p className="card-text">Iso3: {iso3}</p>
 
+                {emissionDataYearRange[0] === 0
+                    ? <p className="card-text">No Country Emission Data Available </p>
+                    : <p className="card-text">Country Emission Data Available From: {emissionDataYearRange[0]} to {emissionDataYearRange[1]} </p>
+
+                }
+
+                {temperatureDataYearRange[0] === 0
+                    ? <p className="card-text">No Temperature Data Available </p>
+                    : <p className="card-text">Temperature Data Available From: {temperatureDataYearRange[0]} to {temperatureDataYearRange[1]} </p>}
+               
                 </div>
         </div>
     );
