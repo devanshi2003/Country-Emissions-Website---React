f8afa8c1a79be8cbb9790f3a50c5fe282af3e8db
Devanshi <devanshiapatel2003@gmail.com>
2023-10-21 11:51:36 +1030

    Update CountriesCard.js

--- Assig2/a2reactapp/src/Components/CountriesCard.js
+++ Assig2/a2reactapp/src/Components/CountriesCard.js
@@ -1,9 +1,11 @@
-const countriesCard = ({ countryName }) => {
+const countriesCard = ({ countryName, regionName }) => {
 
     return (
         <div className="card" style={{ width: '18rem' }}>
                 <div className="card-body">
-                    <h5 className="card-title">{countryName}</h5>
+                <h5 className="card-title">{countryName}</h5>
+                <h6 className="card-title">{regionName}</h6>
+
                 </div>
         </div>
     );
