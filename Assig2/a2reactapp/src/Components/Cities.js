9d8c41a3f48b00adb5cc6beb41d87cfe1b0a1fea
Devanshi <devanshiapatel2003@gmail.com>
2023-10-21 19:25:34 +1030

    Cities.js: Add back btn to Countries

--- Assig2/a2reactapp/src/Components/Cities.js
+++ Assig2/a2reactapp/src/Components/Cities.js
@@ -1,8 +1,19 @@
+import {Link} from "react-router-dom"
+
 const Cities = ({ }) => {
     return (
-        <div className="card col-4 mb-2" style={{ width: 18 + 'rem' }}>
-            <h5 className="card-title">Cities Page</h5>
+        <div>
+            <div class="position-relative pt-5">
+                <div class="position-absolute top-50 start-0 translate-middle">
+                    <Link class="btn btn-primary" to={"/Countries/0"}>Back to Countries</Link>
+                </div>
+            </div>
+
+            <div className="card col-4 mb-2" style={{ width: 18 + 'rem' }}>
+                <h5 className="card-title">Cities Page</h5>
+            </div>
         </div>
+
     )
 }
 export default Cities
