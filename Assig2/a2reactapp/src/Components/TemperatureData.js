a426a96520224176c3eec9548961b927bb74cda7
Devanshi <devanshiapatel2003@gmail.com>
2023-10-22 11:57:17 +1030

    TemperatureData.js: Display the data retrieved by the API in a table

--- Assig2/a2reactapp/src/Components/TemperatureData.js
+++ Assig2/a2reactapp/src/Components/TemperatureData.js
@@ -1,15 +1,13 @@
 import { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 
-
-const TemperatureData = ({ }) => {
-
+const TemperatureData = () => {
     const params = useParams()
     const [temperatureData, updateTemperatureData] = useState({})
 
     useEffect(() => {
         fetch(`http://localhost:5256/api/B_Countries/CountryTemperatureDetail/${params.countryId}`)
-            .then(respose => respose.json())
+            .then(response => response.json())
             .then(data => updateTemperatureData(data))
             .catch(err => {
                 console.log(err)
@@ -18,15 +16,40 @@ const TemperatureData = ({ }) => {
 
     return (
         <div>
-            <div className="card col-4 mb-2" style={{ width: 18 + 'rem' }}>
+            <div className="card col-4 mb-2" style={{ width: '18rem' }}>
                 <h5 className="card-title">Temperature Data Page {params.countryId}</h5>
             </div>
 
-            <div class="position-relative pt-5">
-                <div class="position-absolute top-50 start-0 translate-middle">
-                    <Link class="btn btn-primary" to={"/Countries/" + params.regionId}>Back to Regions</Link>
+            <div className="position-relative pt-5">
+                <div className="position-absolute top-50 start-0 translate-middle">
+                    <Link className="btn btn-primary" to={"/Countries/" + params.regionId}>Back to Regions</Link>
                 </div>
             </div>
+
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Year</th>
+                        <th scope="col">Value</th>
+                        <th scope="col">Unit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                    {temperatureData.rawTemperatureData
+                        ? (
+                            temperatureData.rawTemperatureData.map((data) => (
+                                <tr>
+                                    <td>{data.theCountryTempData.year}</td>
+                                    <td>{data.theCountryTempData.value}</td>
+                                    <td>{data.theCountryTempData.unit}</td>
+                                </tr>
+                            ))
+                        )
+                        : <p>Loading data...</p>
+                    }                 
+                </tbody>
+            </table>
         </div>
     )
 }
