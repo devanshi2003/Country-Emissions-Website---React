79bf817dc604f2bbeb4b7f7e367c1f606e9a4adb
Devanshi <devanshiapatel2003@gmail.com>
2023-10-22 15:46:14 +1030

    Create CountryEmission component with title

---
+++ Assig2/a2reactapp/src/Components/CountryEmission.js
@@ -0,0 +1,12 @@
+const CountryEmission = ({ }) => {
+    return (
+        <div>
+
+            <div className="card col-4 mb-2" style={{ width: '18rem' }}>
+                <h5 className="card-title">Country Emission Data Page </h5>
+            </div>
+
+        </div>
+    )
+}
+export default CountryEmission
