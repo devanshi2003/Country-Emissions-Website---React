bf32726ad7a535689e81b1f39029c177292b920c
Devanshi <devanshiapatel2003@gmail.com>
2023-10-22 11:29:28 +1030

    Create TemperatureData Component with Title

---
+++ Assig2/a2reactapp/src/Components/TemperatureData.js
@@ -0,0 +1,9 @@
+const temperatureData = ({ }) => {
+    return (
+        <div className="card col-4 mb-2" style={{ width: 18 + 'rem' }}>
+            <h5 className="card-title">Temperature Data Page</h5>
+        </div>
+    )
+}
+
+export default temperatureData
