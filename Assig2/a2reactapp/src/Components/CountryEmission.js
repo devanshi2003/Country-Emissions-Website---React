549a584c3d4dde9df2f48b0ec4d24ce7e0169030
Devanshi <devanshiapatel2003@gmail.com>
2023-10-22 20:00:10 +1030

    CountryEmission.js: Display element emission table

--- Assig2/a2reactapp/src/Components/CountryEmission.js
+++ Assig2/a2reactapp/src/Components/CountryEmission.js
@@ -6,8 +6,8 @@ const CountryEmission = ({ }) => {
     const params = useParams();
     const [summaryCountryEmissions, updateSummaryCountryEmissions] = useState([]);
     const [elementData, updateElementData] = useState([]);
-    const [selectedElement, updateSelectedElement] = useState('');
-    const [countryEmission, updateCountryEmission] = useState('');
+    const [selectedElement, updateSelectedElement] = useState(0);
+    const [countryEmission, updateCountryEmission] = useState([]);
 
     useEffect(() => {
         fetch(`http://localhost:5256/api/B_Countries/SummaryCountryEmissionData/${params.countryId}`)
@@ -40,7 +40,8 @@ const CountryEmission = ({ }) => {
     }, [selectedElement]);
 
     function elementChanged(event) {
-        updateSelectedElement(event.target.value)
+        const selectedElement = document.querySelector('[name = "selectedElement"]').value;
+        updateSelectedElement(selectedElement)
     }
 
     return (
@@ -49,14 +50,14 @@ const CountryEmission = ({ }) => {
             <div className="card col-4 mb-2" style={{ width: '18rem' }}>
                 <h5 className="card-title">Country Emission Data Page </h5>
             </div>
-
+                          
             <div class="position-relative pt-5">
                 <div class="position-absolute top-50 start-0 translate-middle">
                     <Link class="btn btn-primary" to={"/Countries/" + params.regionId}>Back to Countries</Link>
                 </div>
             </div>
 
-            <select class="form-select" style={{ width: '300px' }} value={selectedElement} onChange={elementChanged} name="selectedVal">
+            <select class="form-select" style={{ width: '300px' }} value={selectedElement} onChange={elementChanged} name="selectedElement">
                 <option selected>Choose an element</option>
                 {elementData.map((element) => (
                     <option value={element.elementId}>{element.elementName}</option>
@@ -68,13 +69,35 @@ const CountryEmission = ({ }) => {
             <table className="table">
                 <thead>
                     <tr>
+                        <th scope="col">Year</th>
+                        <th scope="col">Item Name</th>
+                        <th scope="col">Value</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        countryEmission ?countryEmission.map((data) => (
+                        <tr>
+                            <td>{data.year}</td>
+                            <td>{data.itemName}</td>
+                            <td>{data.value}</td>
+                            </tr>                       
+                        ))
+                    :<p>Select Element to Load Data</p>
+                }
+
+                </tbody>
+            </table>
+
+            <table className="table">
+                <thead>
+                    <tr>
                         <th scope="col">Element</th>
                         <th scope="col">Year</th>
                         <th scope="col">Total Emission</th>
                     </tr>
                 </thead>
                 <tbody>
-
                     {summaryCountryEmissions.map((data) => (
                         <tr>
                             <td>{data.element}</td>
