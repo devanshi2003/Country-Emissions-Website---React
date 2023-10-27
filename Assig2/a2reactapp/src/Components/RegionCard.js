9f820a186e5c24488bec1373408d899b81792ea2
Devanshi <devanshiapatel2003@gmail.com>
2023-10-27 16:15:41 +1030

    Format RegionCard.js

--- Assig2/a2reactapp/src/Components/RegionCard.js
+++ Assig2/a2reactapp/src/Components/RegionCard.js
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
+import '../site.css';
+
 
 const RegionCard = ({ regionId, regionName, imageUrl, countryCount }) => {
 
 
     return (
         <div className="col-lg-4 mt-5">
-            <div className="card border-0">
-
+            <div className="card border-0 region-card" >
                 {regionId !== 0 && <img src={imageUrl} className="rounded-circle mx-auto mt-3" width="140" height="140" alt={"Image of " + regionName} />}
                 <div className="card-body d-flex flex-column align-items-center">
                     <h3 className="card-title text-center">{regionName}</h3>
