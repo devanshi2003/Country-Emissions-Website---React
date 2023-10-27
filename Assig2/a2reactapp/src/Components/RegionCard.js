c1c773c72626bebbb45790149bcf759718f6034f
Devanshi <devanshiapatel2003@gmail.com>
2023-10-27 12:19:11 +1030

    RegionCard.js: Use stretched-link instead of  image link

--- Assig2/a2reactapp/src/Components/RegionCard.js
+++ Assig2/a2reactapp/src/Components/RegionCard.js
@@ -10,14 +10,14 @@ const RegionCard = ({ regionId, regionName, imageUrl, countryCount }) => {
     return (
         
         <div className="card" style={{ width: '18rem' }}>
-            <Link to = {"/Countries/" + regionId} style={linkStyle}>
+           
                 <img src={imageUrl} className="card-img-top" alt={"Image of " + regionName} />
                 <div className="card-body">
                     <h5 className="card-title">{regionName}</h5>
                     <h6 className="card-title">{regionId}</h6>
                     <p className="card-text">{countryCount}</p>
-                </div>
-            </Link>
+            </div>
+            <Link class="stretched-link" to={"/Countries/" + regionId} style={linkStyle}/>
         </div>
     );
 };
