3574c4846840cd6b901591441098fd5a5dd8cac7
Devanshi <devanshiapatel2003@gmail.com>
2023-10-28 10:18:20 +1030

    Temperature.js: Display country and region info

--- Assig2/a2reactapp/src/Components/TemperatureData.js
+++ Assig2/a2reactapp/src/Components/TemperatureData.js
@@ -25,13 +25,23 @@ const TemperatureData = ({ }) => {
 
     return (
         <div>
-            <div className="card col-4 mb-2" style={{ width: '18rem' }}>
-                <h5 className="card-title">Temperature Data Page {params.countryId}</h5>
-            </div>
 
             <div className="position-relative pt-5">
                 <div className="position-absolute top-50 start-0 translate-middle">
-                    <Link className="btn btn-primary" to={"/Countries/" + params.regionId} state={{ regionData, countryData }}>Back to Countries</Link>
+                    <Link className="btn btn-primary" to={"/Countries/" + params.regionId} state={ regionData }>Back to Countries</Link>
+                </div>
+            </div>
+
+            <div className="row">
+                <div class="full-width-section full-width-section-countries">
+                    <div className="overlay-content">
+                        <h2>
+                            Temperature Data
+                        </h2>
+                        <h4>Country: {countryData.countryName}</h4>
+                        {regionData.regionId !== 0 && <h5>Region: {regionData.regionName}</h5>}
+                        <img src={countryData.imageUrl} class="rounded-circle mx-auto d-block" width="140" height="140" alt={"Image of " + countryData.countryName} />
+                    </div>
                 </div>
             </div>
 
