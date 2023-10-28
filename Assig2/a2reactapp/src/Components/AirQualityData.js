d6cd3bb361325cd3561e2720a72109821e665878
Devanshi <devanshiapatel2003@gmail.com>
2023-10-28 16:29:29 +1030

    FIx: Ensure back btn directs to cities from airQualityData

--- Assig2/a2reactapp/src/Components/AirQualityData.js
+++ Assig2/a2reactapp/src/Components/AirQualityData.js
@@ -36,7 +36,7 @@ const AirQualityData = ({ }) => {
 
             <div className="position-relative pt-5">
                 <div className="position-absolute top-50 start-0 translate-middle">
-                    <Link className="btn btn-primary" to={"/Cities/" + params.countryId + "/" + params.regionId}>Back to Cities </Link>
+                    <Link className="btn btn-primary" to={"/Cities/" + params.countryId + "/" + params.regionId} state={{ regionData, countryData }}>Back to Cities </Link>
                 </div>
             </div>
 
